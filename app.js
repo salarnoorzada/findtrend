ba66203b46576c472c2613c53bbea349ba41c701
Salar Noorzada <nourzada12@gmail.com>
2023-05-24 09:59:51 +0430

    Open tabs animation and hero section animation done

--- app.js
+++ app.js
@@ -12,15 +12,43 @@ burgerMenu.addEventListener("click", () => {
 // ? the below is triggering a className into html when you scroll and reach to that element
 // ? so it makes a scroll animation.
 
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//     } else {
-//       entry.target.classList.remove("show");
-//     }
-//   });
-// });
-// const box = document.querySelectorAll(".findtrend-box");
-// box.forEach((el) => observer.observe(el));
+//! the greenSeciont Title animation on scroll:
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showTitle");
+    } else {
+      entry.target.classList.remove("showTitle");
+    }
+  });
+});
+const title = document.querySelectorAll(".open-tabs-header");
+title.forEach((el) => observer.observe(el));
+//!open-tabs-box:
+const observerTwo = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const box = document.querySelectorAll(".findtrend-box");
+box.forEach((el) => observerTwo.observe(el));
+//! open tabs text:
+
+const observerThree = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showThree");
+    } else {
+      entry.target.classList.remove("showThree");
+    }
+  });
+});
+const boxTwo = document.querySelectorAll(".open-tabs-text");
+boxTwo.forEach((el) => observerThree.observe(el));
