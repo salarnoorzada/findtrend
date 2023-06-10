1b7e12378271a3bc92e37f30c24af9fd29c9c617
Salar Noorzada <nourzada12@gmail.com>
2023-06-10 15:27:39 +0430

    The text animation done

--- app.js
+++ app.js
@@ -52,3 +52,102 @@ const observerThree = new IntersectionObserver((entries) => {
 });
 const boxTwo = document.querySelectorAll(".open-tabs-text");
 boxTwo.forEach((el) => observerThree.observe(el));
+// !!!!
+// var i = 0;
+// var txt =
+//   "Findtrend helps you to increase your productivity and reduce your computer's memory load, "; /* The text */
+// var speed = 30; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   } else {
+//     // add the span element after the text is typed
+//     var span = document.createElement("span");
+//     span.className = "long-text-gray";
+//     span.innerHTML = "an application can<br /> fulfill daily browsing needs.";
+//     document.getElementById("demo").appendChild(span);
+//     // blinkCursor(); // start the cursor effect
+//   }
+// }
+
+// // function blinkCursor() {
+// //   var cursor = document.getElementById("demo");
+// //   cursor.style.borderRight =
+// //     cursor.style.borderRight == "0.15em solid orange"
+// // ? "none"
+// //       : "0.15em solid orange";
+// //   setTimeout(blinkCursor, 750);
+// // }
+
+// typeWriter(); // start the typing effect
+// // !!
+// function isInViewport(element) {
+//   // Get the element's position relative to the viewport
+//   var bounding = element.getBoundingClientRect();
+//   // Check if the element is outside the viewport
+//   // Then invert the returned value because you want to know the opposite
+//   return !(bounding.top > innerHeight || bounding.bottom < 0);
+// }
+
+// // Listen for the scroll event
+// window.addEventListener("scroll", function () {
+//   // Check if the element is in the viewport
+//   if (isInViewport(document.getElementById("demo"))) {
+//     // Start the typing effect
+//     typeWriter();
+//     // Remove the scroll event listener so it doesn't repeat
+//     window.removeEventListener("scroll", arguments.callee);
+//   }
+// });
+var i = 0;
+var txt =
+  "Findtrend helps you to increase your productivity and reduce your computer's memory load, "; /* The text */
+var speed = 35; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    // add the span element after the text is typed
+    var span = document.createElement("span");
+    span.className = "long-text-gray";
+    span.innerHTML = "an application can<br /> fulfill daily browsing needs.";
+    document.getElementById("demo").appendChild(span);
+    blinkCursor(); // start the cursor effect
+  }
+}
+
+// function blinkCursor() {
+//   var cursor = document.getElementById("demo");
+//   cursor.style.borderRight = cursor.style.borderRight == "0.15em solid orange" ? "none" : "0.15em solid orange";
+//   setTimeout(blinkCursor, 750);
+// }
+
+function isInViewport(element) {
+  // Get the element's position relative to the viewport
+  var bounding = element.getBoundingClientRect();
+  // Check if the element is fully visible in the viewport
+  return bounding.top >= 0 && bounding.bottom <= innerHeight;
+}
+
+// ! the long-text-scroll-animation:
+// Listen for the scroll event
+window.addEventListener("scroll", function () {
+  // Check if the element is in the viewport
+  if (isInViewport(document.getElementById("demo"))) {
+    // Check if the element is hidden
+    if (document.getElementById("demo").innerHTML == "") {
+      // Reset the index and start the typing effect again
+      i = 0;
+      typeWriter();
+    }
+  } else {
+    // Hide the element
+    document.getElementById("demo").innerHTML = "";
+  }
+});
